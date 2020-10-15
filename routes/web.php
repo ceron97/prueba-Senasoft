7a257d01845520c5a2a52c4434040ba3cd43cef9
unknown <jppaz16@misena.edu.co>
2020-10-15 01:24:37 -0500

    se soluciono conflictos de web

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuariosController;
 use App\Http\Controllers\VentasController;
 use App\Http\Controllers\SucursalesController;
-<<<<<<< HEAD
-use App\Http\Controllers\BodegaController;
-=======
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\EmpresasController;
 
->>>>>>> 8199eceb7daca29dd2dd70977acdbc10ee046734
 
 /*
 |--------------------------------------------------------------------------
@@ -42,11 +38,11 @@ Route::get('/adicionar', function () {
     return view('adicionar');
 })->name('adicionar');
 
-<<<<<<< HEAD
-Route::get('/sucursales', [SucursalesController::class, 'index'])->name('sucursales');
+
+
 
 Route::resource('bodega', BodegaController::class);
-=======
+
 Route::resource('sucursales', SucursalesController::class);// se llama todos los recuersos de la vista 
 Route::resource('ventas', VentasController::class);// se llama todos los recuersos de la vista 
 
@@ -57,4 +53,4 @@ Route::get('/admin/usuarios', [AdminController::class, 'usuarios'])->name('usuar
 Route::get('/empresa/usuarios', [EmpresasController::class, 'usuarios'])->name('usuarios.empresa');
 
 Route::resource('empresas', EmpresasController::class);
->>>>>>> 8199eceb7daca29dd2dd70977acdbc10ee046734
+
